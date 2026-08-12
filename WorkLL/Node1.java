5f5d950071fdb802f4fc40bcc142038251acc008
jomplaza555 <s68122250020@ssru.ac.th>
2026-08-12 22:29:38 +0700

    WorkLL

---
+++ WorkLL/Node1.java
@@ -0,0 +1,76 @@
+package Program_Lab_LL_InClass;
+
+class Node1<T> {
+    public T nodeValue; 
+    public Node1<T> next;
+
+    public Node1() {
+        nodeValue = null;
+        next = null;
+    }
+
+    public Node1(T item) {
+        nodeValue = item;
+        next = null;
+    }
+
+    // 1. Method ในการค้นหา node ที่มีค่าตามกำหนด
+    public static <T> Node1<T> searchNode(Node1<T> head, T target) {
+        Node1<T> current = head;
+        while (current != null) {
+            if (current.nodeValue != null && current.nodeValue.equals(target)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    // 2. Method ในการนับจำนวน node ใน list
+    public static <T> int countNodes(Node1<T> head) {
+        int count = 0;
+        Node1<T> current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Node1<String> p = new Node1<String>("red");
+        System.out.println("p = " + p);
+        System.out.println("p.nodeValue = " + p.nodeValue);
+        System.out.println("p.next before = " + p.next);
+
+        Node1<String> q = new Node1<String>("green");
+        Node1<String> m = new Node1<String>("pink");
+        Node1<String> n = new Node1<String>("yellow");
+
+        System.out.println("q = " + q);
+        System.out.println("q.nodeValue = " + q.nodeValue);
+        System.out.println("q.next = " + q.next);
+
+        p.next = n;
+        n.next = q;
+        System.out.println("p.next After = " + p.next);
+        System.out.println("n.next After = " + n.next);
+
+        Node1<String> head = p;
+        System.out.println("head. = " + head);
+
+        q.next = m;
+        System.out.println("q.next After = " + q.next);
+
+        Node1<String> tail = m; 
+        System.out.println("tail. = " + tail);
+
+        // หา node ที่มีค่าเป็น "blue" ใน linked list
+        Node1<String> targetNode = searchNode(head, "blue");
+        System.out.println("search 'blue' = " + targetNode);
+
+        // นับจำนวน node ใน list
+        int totalNodes = countNodes(head);
+        System.out.println("count nodes = " + totalNodes);
+    }
+}
